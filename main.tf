1ce489c5d4dcc57cd17977cd3ee1aa28f294af03
Lance Haig <lnhaig@gmail.com>
2019-12-03 16:44:15 +0100

    Upgrade to 0.12

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-terraform {
-  required_version = ">= 0.11.7"
-}
-
 variable "location" {
   description = "Azure location in which to create resources"
-  default = "East US"
+  default     = "East US"
 }
 
 variable "windows_dns_prefix" {
@@ -13,43 +9,45 @@ variable "windows_dns_prefix" {
 
 variable "admin_password" {
   description = "admin password for Windows VM"
-  default = "pTFE1234!"
+  default     = "pTFE1234!"
 }
 
 variable "vm_size" {
   description = "size of the Azure VM"
-  default = "Standard_A1"
+  default     = "Standard_A1"
 }
 
 variable "storage_account_type" {
   description = "Defines the type of storage account: Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS"
-  default = "Standard_LRS"
+  default     = "Standard_LRS"
 }
 
-provider "azurerm" {}
+provider "azurerm" {
+}
 
 module "windowsserver" {
-  source              = "Azure/compute/azurerm"
-  version             = "1.1.5"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  vm_hostname         = "demohost"
-  admin_password      = "${var.admin_password}"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  storage_account_type = "${var.storage_account_type}"
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  vm_size             = "${var.vm_size}"
+  source               = "Azure/compute/azurerm"
+  version              = "1.3.0"
+  location             = var.location
+  resource_group_name  = "${var.windows_dns_prefix}-rc"
+  vm_hostname          = "demohost"
+  admin_password       = var.admin_password
+  vm_os_simple         = "WindowsServer"
+  public_ip_dns        = [var.windows_dns_prefix]
+  storage_account_type = var.storage_account_type
+  vnet_subnet_id       = module.network.vnet_subnets[0]
+  vm_size              = var.vm_size
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "1.1.1"
-  location            = "${var.location}"
+  location            = var.location
   resource_group_name = "${var.windows_dns_prefix}-rc"
   allow_ssh_traffic   = true
 }
 
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+output "windows_vm_public_name" {
+  value = module.windowsserver.public_ip_dns_name
 }
+
